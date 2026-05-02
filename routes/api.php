a78d809141ed0349c081e6e2178f77377586e173
Kanchnak Khorn <kanchnakkhorn@gmail.com>
2026-05-02 14:59:33 +0700

    init game table + create game

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\GameController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
@@ -8,3 +9,4 @@ Route::post('/signup', [UserController::class, 'createUser']);
 Route::post('/login', [UserController::class, 'login']);
 
 Route::post('/categories', [CategoryController::class, 'create'])->middleware('auth:sanctum');
+Route::post('/games', [GameController::class, 'create'])->middleware('auth:sanctum');
