c861ac618b78dc0a7ce3fb0d42f1864f3574e6c7
CheaChandyna <cheachandynaa@gmail.com>
2026-05-04 11:11:25 +0700

    feat: create endpoint for update user, reset password and group protected routes

--- routes/api.php
+++ routes/api.php
@@ -8,5 +8,10 @@ use Illuminate\Support\Facades\Route;
 Route::post('/signup', [UserController::class, 'createUser']);
 Route::post('/login', [UserController::class, 'login']);
 
-Route::post('/categories', [CategoryController::class, 'create'])->middleware('auth:sanctum');
-Route::post('/games', [GameController::class, 'create'])->middleware('auth:sanctum');
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/categories', [CategoryController::class, 'create']);
+    Route::post('/games', [GameController::class, 'create']);
+
+    Route::put('/user/update', [UserController::class, 'update']);
+    Route::put('/user/reset-password', [UserController::class, 'updatePassword']);
+});
